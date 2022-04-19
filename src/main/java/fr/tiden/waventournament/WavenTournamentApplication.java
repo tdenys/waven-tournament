c3d1cf3cea27fe7e10a54d776b8ad42d45513303
titou <titouan.denys@gmail.com>
2022-04-19 20:19:09 +0200

    first commit

---
+++ src/main/java/fr/tiden/waventournament/WavenTournamentApplication.java
@@ -0,0 +1,13 @@
+package fr.tiden.waventournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  WavenTournamentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WavenTournamentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/tiden/waventournament/WavenTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tiden.waventournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavenTournamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
